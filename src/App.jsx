16342ccb197ccc5361939a0754aec27d98152e99
Ananya54321 <ananyapappula2004@gmail.com>
2024-07-17 19:01:36 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,52 @@
-import Button from "./components/Button.jsx"
-import React from "react"
-import Navbar from "./components/Navbar.jsx"
+"use client";
+import { useState, useEffect } from "react";
+import Button from "./components/Button.jsx";
+import Navbar from "./components/Navbar.jsx";
+import working from "./assets/working.svg";
 
 const App = () => {
+  const [data, setData] = useState([]);
+
+  async function fetchData() {
+    try {
+      const response = await fetch('https://dummyjson.com/products');
+      const data = await response.json();
+      return data?.products ?? [];
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      return [];
+    }
+  }
+
+  useEffect(() => {
+    async function getData() {
+      const fetchedData = await fetchData();
+      setData(fetchedData);
+      console.log(fetchedData); // Check fetched data in console
+    }
+    getData();
+  }, []);
 
   return (
     <div className="bg-[#0075A7] w-full h-screen">
       <Navbar />
-      <p className="rancho text-3xl md:text-5xl">Hello There, I'm Ananya!</p>
-      <p className="pt-[1.5rem]">I am a full stack web developer who is passionate about creating impactful projects and finds peace in creating digital art. </p>
-      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden" >
-        <Button />
+      <div className="flex justify-between w-full items-center">
+        <div className="m-10 ml-20 w-[40%] flex flex-col">
+          <p className="rancho text-3xl md:text-7xl">Hello There, I'm Ananya!</p>
+          <p className="pt-[1.5rem] text-2xl imprima">
+            I am a full stack web developer who is passionate about creating
+            impactful projects and finds peace in creating digital art.
+          </p>
+          <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+            <Button />
+          </div>
+        </div>
+        <div className="m-20">
+          <img src={working} alt="a woman working on code and sipping tea" />
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -6,7 +6,7 @@ import '../index.css'
 function Button() {
   return (
   <AwesomeButton 
-  className="rancho"
+  className="rancho text-3xl"
     type="primary"
     href = "https://www.linkedin.com/in/ananya-pappula-1735b9288/"
     
